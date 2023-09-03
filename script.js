82d9fcffb9365cee5ce7a9f3e047137372b588f5
Kevwe Gloria Okpoh <Kevweglo@gmail.com>
2023-09-03 14:20:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// Add event listener to hide the loader when the page is fully loaded
-window.addEventListener("load", function () {
-    const loaderWrapper = document.querySelector(".loader-wrapper");
-    loaderWrapper.style.transition = "opacity 0.5s";
-    loaderWrapper.style.opacity = "0";
-    setTimeout(function () {
-        loaderWrapper.style.display = "none";
-    }, 10000);
-    // Add the "loaded" class to the body to apply styles to hide the loader
-    document.body.classList.add("loaded");
-});
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+    }, 3000)
+  });
+
+  
